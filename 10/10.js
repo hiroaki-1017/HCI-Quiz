411aaed242a5634454bea8a49b1720c04f52c398
kspace-trk <keigonakao.0825@gmail.com>
2019-11-18 00:08:24 +0900

    マルバツ表示修正

--- 10/10.js
+++ 10/10.js
@@ -1,6 +1,8 @@
 function answerY(){
  
- 
+    var img = document.getElementById("answer");
+    img.src="../img/yes.png";
+
     document.getElementById("changeTextColorr").style.color = ("#29FF00");
 
 }
@@ -12,5 +14,3 @@ function answerN(){
 
 }
 
-var img = document.getElementById("answer");
-img.src="../img/yes.png";
