13a6868459bd3772037912c60ba3d069803c60fa
kspace-trk <keigonakao.0825@gmail.com>
2019-11-17 23:40:35 +0900

    textcolor変えるの詰んでる

---
+++ 10/10.js
@@ -0,0 +1,16 @@
+function answerY(){
+ 
+ 
+    document.getElementById("changeTextColorr").style.color = ("#29FF00");
+
+}
+
+function answerN(){
+
+    var img = document.getElementById("answer");
+    img.src="../img/no.png";    
+
+}
+
+var img = document.getElementById("answer");
+img.src="../img/yes.png";
