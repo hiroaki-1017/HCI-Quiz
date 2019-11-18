f4be1de9cf0717125be1ea7753be19b3f65c6c2a
REiJi <shino327gh@gmail.com>
2019-11-19 00:07:53 +0900

    Update 10.js

--- 10/10.js
+++ 10/10.js
@@ -1,18 +1,18 @@
 function answerY(){
  
-    var img = document.getElementsByClassName("answer");
+    var img = document.getElementsByClassName('answer');
     img.src="../img/yes.png";
 
-    document.getElementsByClassName("changeTextColor").style.color = ("#29FF00");
+    document.getElementsByClassName('changeTextColor').style.color = ("#29FF00");
 
 }
 
 function answerN(){
 
-    var img = document.getElementsByClassName("eanswr");
+    var img = document.getElementsByClassName('eanswr');
     img.src="../img/no.png";
 
-    document.getElementsByClassName("changeTextColor").style.color = ("#29FF00");
+    document.getElementsByClassName('changeTextColor').style.color = ("#29FF00");
 
 }
 
